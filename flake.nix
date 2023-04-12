21417ee0bf81d3c68460cc258ad872b03519bd3b
Marc Lacoursiere <marc@roosoft.com>
2023-04-12 19:42:31 -0400

    working version with a plain neovim

--- flake.nix
+++ flake.nix
@@ -2,19 +2,23 @@
   description = "Neovim flake";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    neovim = import ./.;
+    # neovim = import "./.";
   };
 
   outputs = {
     self,
     nixpkgs,
     nixpkgs_unstable,
-    neovim,
-  }: {
-    packages.x86_64-linux.default = neovim;
+    # neovim,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    packages.x86_64-linux.default = pkgs.neovim;
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = [pkgs.neovim];
+    };
   };
 }
