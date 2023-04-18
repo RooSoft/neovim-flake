fc100031080e1c2ee869b3962ff6cbda0b3c2791
Marc Lacoursiere <marc@roosoft.com>
2023-04-18 11:36:29 -0400

    make sure it works on every architecture

--- flake.nix
+++ flake.nix
@@ -5,26 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # neovim = import "./.";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
     nixpkgs_unstable,
-# neovim,
-  }: let
-    neovim_overlay = import ./neovim;
+    flake-utils
+  }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      neovim_overlay = import ./neovim;
 
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      overlays = [neovim_overlay];
-      config.allowUnfreePredicate = true;
-    };
-  in {
-    packages.x86_64-linux.default = pkgs.neovim;
-    devShell.x86_64-linux = pkgs.mkShell {
-      buildInputs = [pkgs.neovim];
-    };
-  };
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      overlays.default = neovim_overlay;
+      packages = pkgs.neovim;
+      devShell = pkgs.mkShell {
+        buildInputs = [pkgs.neovim];
+      };
+    }
+  );
 }
